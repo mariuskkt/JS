c7e8e97075e15bcb0c0b4113a08fda64f3577810
mariuskkt <marius.kukta@gmail.com>
2020-05-04 10:04:23 +0200

    JS first day

---
+++ script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// console.log(window.innerHeight);
+//
+// console.log(alert('Shiiiit'));
+// console.log(prompt('Your name:'));
+
+let name = "Marius";
+name = 13;
+
+const age = 100;
+const array = [
+    'gvazdikas'
+];
+
+const obj = {
+    name: 'vardas',
+    age: 20
+};
+
+console.log(array[0]);
+console.log(obj);
+console.log(name);
+console.log(age);
