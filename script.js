3842d52177ad8d146e309a66e6a20cfbe089f8bf
mariuskkt <marius.kukta@gmail.com>
2020-05-04 12:41:03 +0200

    random first day stuff

--- script.js
+++ script.js
@@ -8,7 +8,14 @@
 let name = "Marius";
 name = 13;
 
+let iq = '100';
+
+iq = Number(iq);
+console.log(iq);
+
 const age = 100;
+
+
 const array = [
     'gvazdikas'
 ];
@@ -22,3 +29,35 @@ console.log(array[0]);
 console.log(obj);
 console.log(name);
 console.log(age);
+
+let result;
+
+function isVisible() {
+    iq < 100 ? result = 'dolbas' : result = 'normalus';
+}
+
+isVisible();
+
+console.log(result);
+
+function sum(a, b) {
+    return a + b;
+}
+
+// console.log(sum(10,50));
+
+
+// input > 10 ? alert('Skaicius per didelis') : alert('ok');
+
+
+let rand_numb = Math.floor(Math.random() * 11);
+
+console.log(rand_numb);
+
+let input = prompt('Iveskite skaiciu:');
+
+while (Number(input) !== rand_numb) {
+    input = prompt('bandyk dar:')
+}
+
+alert("Atspejai");
