cfe743cd2d6e42aad352967249260b7684ba6298
mariuskkt <marius.kukta@gmail.com>
2020-05-05 10:05:38 +0200

    JS for in

--- script.js
+++ script.js
@@ -1,63 +1,23 @@
 'use strict';
 
-// console.log(window.innerHeight);
-//
-// console.log(alert('Shiiiit'));
-// console.log(prompt('Your name:'));
-
-let name = "Marius";
-name = 13;
-
-let iq = '100';
-
-iq = Number(iq);
-console.log(iq);
-
-const age = 100;
-
-
-const array = [
-    'gvazdikas'
-];
-
-const obj = {
-    name: 'vardas',
-    age: 20
+const shoppingCart = {
+    milk: 2,
+    beer: 3
 };
 
-console.log(array[0]);
-console.log(obj);
-console.log(name);
-console.log(age);
 
-let result;
+let question;
 
-function isVisible() {
-    iq < 100 ? result = 'dolbas' : result = 'normalus';
+do {
+    const name = prompt('Write in name of a product:');
+    const quantity = prompt('Write in quantity:');
+
+    shoppingCart[name] = Number(quantity);
+
+    question = prompt('Do you want to add one more item?Answer \'Y\' for yes');
+} while (question === 'Y');
+
+for (const name in shoppingCart) {
+    alert(name + ' quantity ' + shoppingCart[name])
 }
-
-isVisible();
-
-console.log(result);
-
-function sum(a, b) {
-    return a + b;
-}
-
-// console.log(sum(10,50));
-
-
-// input > 10 ? alert('Skaicius per didelis') : alert('ok');
-
-
-let rand_numb = Math.floor(Math.random() * 11);
-
-console.log(rand_numb);
-
-let input = prompt('Iveskite skaiciu:');
-
-while (Number(input) !== rand_numb) {
-    input = prompt('bandyk dar:')
-}
-
-alert("Atspejai");
+console.log(shoppingCart);
