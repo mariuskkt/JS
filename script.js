f60070f55b6c435e2da7e8b2af3c918cbbbe9383
mariuskkt <marius.kukta@gmail.com>
2020-05-11 19:16:05 +0200

    invisible functions

--- script.js
+++ script.js
@@ -1,50 +1,48 @@
 'use strict';
 
-// const text = 'yellow diamond ring on my fist like a pineapple\n' +
-//     'Shaped like a coke bottle, so I threw a dime on her (Damn)\n' +
-//     'Shine on her, grind on her, I might wine and dine her (Yeah)';
+const input = document.querySelector('input[name = word]');
+const h1 = document.querySelector('h1');
+let name;
 
-const word = 'pineapple';
-const input = document.querySelector('input[name=todo]');
-const edit = document.querySelector('input[name=edit]');
-const ul = document.querySelector('ul');
-const h1 = document.createElement('h1');
+const vowelsArray = [
+    'a', 'e', 'o', 'u', 'i'
+];
+
 input.addEventListener('keyup', onKey);
-edit.style.display = 'none';
-
-function capitalizeWord(word) {
-    // let letters = word.split('');
-    // let upper = letters[0].toUpperCase();
-    // letters.splice(0, 1, upper);
-    // return letters.join('');
-
-    return word.charAt(0).toUpperCase() + word.slice(1);
-}
 
 function onKey(event) {
     if (event.keyCode === 13 && input.value.trim()) {
-        const li = createListElement(input.value.trim());
-        ul.appendChild(li);
+        createHeaderElement(input.value.trim());
 
-        input.value = "";
+        input.style.display = 'none';
+        name = input.value.trim();
+        h1.innerHTML = name;
     }
 }
 
-function createListElement(input) {
-    const li = document.createElement("li");
-    const inputText = document.createTextNode(input);
-    const span = document.createElement('span');
+function createHeaderElement(input) {
+    const div = document.createElement("div");
 
-    const buttonDelete = createButton('Delete', deleteItem);
-    const buttonEdit = createButton('Edit', editItem);
+    const buttonVowels = createButton('Vowels', vowels => {
+        const filtered = name
+            .split('')
+            .filter(letter => vowelsArray.find(vowel => letter.toLowerCase() === vowel))
+            .join('');
+        h1.innerHTML = filtered;
+    });
+    const buttonConsonants = createButton('Consonants', consonants => {
+        const filtered = name
+            .split('')
+            .filter(letter => !vowelsArray.find(vowel => letter.toLowerCase() === vowel))
+            .join('');
+        h1.innerHTML = filtered;
+    });
+    const buttonFullName = createButton('Full name', fullName);
 
-    span.appendChild(inputText);
-    li.appendChild(span);
-
-    li.appendChild(buttonEdit);
-    li.appendChild(buttonDelete);
-
-    return li;
+    div.appendChild(buttonVowels);
+    div.appendChild(buttonConsonants);
+    div.appendChild(buttonFullName);
+    document.body.appendChild(div);
 }
 
 function createButton(text, handler) {
@@ -57,42 +55,22 @@ function createButton(text, handler) {
     return genericButton;
 }
 
-function deleteItem(event) {
-    const closestLi = event.target.closest('li');
+// const vowels = () => {
+//     const filtered = name
+//         .split('')
+//         .filter(letter => vowelsArray.find(vowel => letter.toLowerCase() === vowel))
+//         .join('');
+//     h1.innerHTML = filtered;
+// };
+//
+// const consonants = () => {
+//     const filtered = name
+//         .split('')
+//         .filter(letter => vowelsArray.find(vowel => letter.toLowerCase() === vowel))
+//         .join('');
+//     h1.innerHTML = filtered;
+// }
 
-    function deleteLiItem() {
-        closestLi.remove();
-    }
-
-    setTimeout(deleteLiItem, 1000);
-    closestLi.style.opacity = '0';
-    closestLi.style.transition = '1s';
-    closestLi.style.transform = 'translateX(250%)';
+function fullName() {
+    h1.innerHTML = name;
 }
-
-function editItem(event) {
-    const listItem = event.target.closest('li');
-
-    function getEditInput(event) {
-
-        if (event.keyCode === 13 && edit.value.trim()) {
-            const span = document.createElement('span');
-            const text = document.createTextNode(edit.value);
-            span.appendChild(text);
-
-            listItem.firstChild.replaceWith(span);
-
-            edit.removeEventListener('keyup', getEditInput);
-            edit.style.display = 'none';
-        }
-    }
-
-    edit.value = listItem.firstChild.textContent;
-    edit.style.display = 'inline-block';
-    edit.addEventListener('keyup', getEditInput);
-    edit.focus();
-    edit.select();
-}
-
-// setInterval();
-// setTimeout();
