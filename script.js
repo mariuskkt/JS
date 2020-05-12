162b9f04217e260ec2619d037004ba6e75f79464
mariuskkt <marius.kukta@gmail.com>
2020-05-12 09:44:01 +0200

    chat bubble

--- script.js
+++ script.js
@@ -1,76 +1,41 @@
 'use strict';
 
-const input = document.querySelector('input[name = word]');
-const h1 = document.querySelector('h1');
-let name;
+const textarea = document.querySelector('textarea');
+const button = document.querySelector('button');
+const main = document.querySelector('main');
 
-const vowelsArray = [
-    'a', 'e', 'o', 'u', 'i'
-];
+button.addEventListener('click', onClick);
 
-input.addEventListener('keyup', onKey);
+function onClick(event) {
+    const article = document.createElement('article');
+    const image = document.createElement('section');
+    const chatBubble = document.createElement('section');
+    const paragraph = document.createElement('p');
+    const value = textarea.value.trim();
+    const arrayParagraph = textarea.value.split('\n');
 
-function onKey(event) {
-    if (event.keyCode === 13 && input.value.trim()) {
-        createHeaderElement(input.value.trim());
+    arrayParagraph.forEach(para => {
+        const paragraph = document.createElement('p');
+        const textNode = document.createTextNode(para);
+        paragraph.appendChild(textNode);
+        chatBubble.appendChild(paragraph);
+    });
 
-        input.style.display = 'none';
-        name = input.value.trim();
-        h1.innerHTML = name;
+    article.className = 'message self';
+    image.className = 'image';
+    chatBubble.className = 'chat-bubble';
+    paragraph.innerHTML = value;
+
+    // article.classList.add('message', 'self');
+    // image.classList.add('image');
+    // image.classList.add('chat-bubble');
+    // paragraph.innerHTML = value;
+
+    if (value) {
+        article.appendChild(image);
+        article.appendChild(chatBubble);
+        main.appendChild(article);
     }
-}
 
-function createHeaderElement(input) {
-    const div = document.createElement("div");
-
-    const buttonVowels = createButton('Vowels', vowels => {
-        const filtered = name
-            .split('')
-            .filter(letter => vowelsArray.find(vowel => letter.toLowerCase() === vowel))
-            .join('');
-        h1.innerHTML = filtered;
-    });
-    const buttonConsonants = createButton('Consonants', consonants => {
-        const filtered = name
-            .split('')
-            .filter(letter => !vowelsArray.find(vowel => letter.toLowerCase() === vowel))
-            .join('');
-        h1.innerHTML = filtered;
-    });
-    const buttonFullName = createButton('Full name', fullName);
-
-    div.appendChild(buttonVowels);
-    div.appendChild(buttonConsonants);
-    div.appendChild(buttonFullName);
-    document.body.appendChild(div);
-}
-
-function createButton(text, handler) {
-    const genericButton = document.createElement('button');
-    const buttonText = document.createTextNode(text);
-
-    genericButton.appendChild(buttonText);
-    genericButton.addEventListener('click', handler);
-
-    return genericButton;
-}
-
-// const vowels = () => {
-//     const filtered = name
-//         .split('')
-//         .filter(letter => vowelsArray.find(vowel => letter.toLowerCase() === vowel))
-//         .join('');
-//     h1.innerHTML = filtered;
-// };
-//
-// const consonants = () => {
-//     const filtered = name
-//         .split('')
-//         .filter(letter => vowelsArray.find(vowel => letter.toLowerCase() === vowel))
-//         .join('');
-//     h1.innerHTML = filtered;
-// }
-
-function fullName() {
-    h1.innerHTML = name;
+    textarea.value = '';
 }
