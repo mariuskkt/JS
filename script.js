fae7c8c01ca1fee9062dbfcc8e5a7bd61a4c4235
mariuskkt <marius.kukta@gmail.com>
2020-05-15 08:33:50 +0200

    cars

--- script.js
+++ script.js
@@ -1,58 +1,81 @@
 'use strict';
 
-const carPic = document.querySelector('img');
-const horn = new Audio('car+horn+x.mp3');
-const engine = new Audio('Corvette+pass.mp3');
+const merc = document.querySelector('.merc');
+const golf = document.querySelector('.golf');
+const beetle = document.querySelector('.beetle');
 
-const car = {
-    positionX: 0,
-    positionY: 0,
-    rotation: 0,
-    moveLeft() {
-        this.positionX -= 10;
-        this.rotation = 0;
-    },
-    moveUp() {
-        this.positionY -= 10;
-        this.rotation = 90;
-    },
-    moveRight() {
-        this.positionX += 10;
-        this.rotation = 180;
-    },
-    moveDown() {
-        this.positionY += 10;
-        this.rotation = 270;
-    },
-    render() {
-        carPic.style.left = this.positionX + 'px';
-        carPic.style.top = this.positionY + 'px';
-        carPic.style.transform = 'rotate(' + this.rotation + 'deg)';
-        engine.play();
-    },
-    move(event) {
-        if (event.keyCode === 87) {
-            car.moveUp();
-            car.render();
-        }
-        if (event.keyCode === 83) {
-            car.moveDown();
-            car.render();
-        }
-        if (event.keyCode === 68) {
-            car.moveRight();
-            car.render();
-        }
-        if (event.keyCode === 65) {
-            car.moveLeft();
-            car.render();
-        }
-        if (event.keyCode === 32) {
-            horn.play();
-        }
-    }
+const velocity = {
+    slow: 10,
+    medium: 20,
+    fast: 30
 };
 
-document.addEventListener('keydown', car.move);
+function constructCar(car, speed, y = 0, rotation = 180) {
 
+    this.positionX = 0;
+    this.positionY = y;
+    this.rotation = rotation;
+    this.car = car;
 
+    this.moveLeft = function () {
+        this.positionX -= speed;
+        this.rotation = 0;
+        return this
+    },
+        this.moveUp = function () {
+            this.positionY -= speed;
+            this.rotation = 90;
+            return this
+        },
+        this.moveRight = function () {
+            this.positionX += speed;
+            this.rotation = 180;
+            return this
+        }
+        ,
+        this.moveDown = function () {
+            this.positionY += speed;
+            this.rotation = 270;
+            return this
+        }
+        ,
+        this.render = function () {
+            this.car.style.left = this.positionX + 'px';
+            this.car.style.top = this.positionY + 'px';
+            this.car.style.transform = 'rotate(' + this.rotation + 'deg)';
+        }
+        ,
+        this.move = function (event) {
+            switch (event.keyCode) {
+                case 87:
+                    this.moveUp();
+                    break;
+                case 68:
+                    this.moveRight();
+                    break;
+                case 65:
+                    this.moveLeft();
+                    break;
+                case 83:
+                    this.moveDown();
+                    break;
+                case 13:
+                    this.pedalToTheMetal();
+                    break;
+            }
+            return this;
+        };
+    this.pedalToTheMetal = function () {
+        while (this.positionX < document.body.clientWidth - 300) {
+            this.moveRight()
+        }
+    };
+    document.addEventListener('keydown', e => {
+        this.move(e)
+        this.render();
+    });
+    this.render();
+}
+
+const car = new constructCar(merc, velocity.slow, 300);
+const mer = new constructCar(golf, velocity.fast, 700);
