d3de8dee76293d170ed6e9fdfeea950337112559
mariuskkt <marius.kukta@gmail.com>
2020-05-18 14:14:19 +0200

    cars with different controllers + webpack config

--- src/index.js
+++ src/index.js
@@ -3,11 +3,5 @@
 import {Mercedes} from './Mercedes'
 import {Ferrari} from "./Ferrari";
 
-const velocity = {
-    slow: 10,
-    medium: 20,
-    fast: 30
-};
-
 const mer = new Mercedes();
 const fer = new Ferrari();
